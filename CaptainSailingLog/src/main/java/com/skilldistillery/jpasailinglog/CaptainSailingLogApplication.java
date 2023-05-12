d15f55ae3808cee9c0685b5741b1055ab559321a
jennkal <jennkalkowski@gmail.com>
2023-05-12 19:09:12 -0400

    Create Spring Boot MVC project, add JPA project dependency

---
+++ CaptainSailingLog/src/main/java/com/skilldistillery/jpasailinglog/CaptainSailingLogApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpasailinglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptainSailingLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptainSailingLogApplication.class, args);
+	}
+
+}
